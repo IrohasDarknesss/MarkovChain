7b7c39018d6306f045f9311771c0c79b665c9dd6
IrohasDarknesss <kaine0401mario@gmail.com>
2022-10-18 19:48:34 +0900

    delete

--- nijigasaki.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists nijigasaki;
-create table nijigasaki (
-    id integer primary key autoincrement not null,
-    prefix1 text not null,
-    prefix2 text not null,
-    suffix text not null,
-    freq integer not null
-);
